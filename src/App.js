f91fb734ee627e15780c1194dc3f647b29066cdb
Hamza Saleem <saleemh075@gmail.com>
2024-03-23 17:29:39 +0500

    delete extra file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 // import About from './components/About';
 import TextForm from './components/TextForm';
 import Navbar from './components/navbar';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [mode, setmode] = useState('light');
+
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode}/>
     <div className="container my-3">
       <TextForm heading="Enter the text to analyze"/>
       {/* <About/> */}
