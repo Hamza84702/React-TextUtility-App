569c5da39bc7381352eac50fff5d06593f75a844
Hamza Saleem <saleemh075@gmail.com>
2024-03-27 17:53:03 +0500

    add the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import TextForm from './components/TextForm';
 import Navbar from './components/navbar';
 import React, { useState } from 'react'
-
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';function App() {
 
   const [mode, setmode] = useState('light');
 
@@ -41,14 +40,19 @@ function App() {
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+      <Routes>
+            <Route exact path="/about" element={<About />}>
+              {/* <About /> */}
+            </Route>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
 
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert}/>
-    <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
-
-      {/* <About/> */}
-    </div>
+      </Routes>
+      </div>
+    </Router>
     </>
   );
 }
